8866e35e600238752300bb67a07b5ee693be785f
Dewni Lokuge <sdewnilokuge@gmail.com>
2018-04-16 04:15:50 -0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -13,13 +13,22 @@ module.exports = (app)=>{
     });
     
     
-        app.post('/user/register',function(req,res){
+    app.post('/user/register',function(req,res){
         var rider_regNo = req.body.rider_regNo;
         var rider_password = req.body.rider_password;
     
         register.register(rider_regNo,rider_password,function (found) {
             res.json(found);
         });
+    });
+    
+    app.get('/user/unlock',function(req,res){
+      var jwt_token = req.body.token;
+      var bike_id=req.body.bike_id;
+
+      unlock.unlock(jwt_token,bike_id,function(found){
+        res.json(found);
+      });
     });
     
 }
