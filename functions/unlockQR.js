c432785f5a474d90ee3712a097aac509f433905a
Puwasuru Ihalagedara <puwasuru95@gmail.com>
2018-05-26 23:48:03 +0530

    user could't get a bike until he return the bike

--- functions/unlockQR.js
+++ functions/unlockQR.js
@@ -11,32 +11,36 @@ var unlockQR = (body) => {
             {"rider_regNo" : regNo}
         ).then((doc) => {
             if(doc){
-                mongoOp_station.findOne(
-                    {"locks.lock_id" : lockId},
-                    {"locks.$.bike_id" : true}
-                ).then((doc) => {
-                    if(doc.locks[0].empty){
-                        reject({'response' : "unsuccess lock is already empty", 'res' : false});
-                    }else{
-                        mongoOp_station.findOneAndUpdate(
-                            {"locks.lock_id" : lockId},
-                            {$set : {"locks.$.bike_id" : "null" , "locks.$.empty" : true }, $inc:{'noOfEmpty' : 1, 'noOfBikes' : -1}},
-                            {upsert : true , fields : {"locks.$.bike_id" : 1, "station_id" : 1}}
-                        ).then((doc) => {
-                            mongoOp_bike.findOneAndUpdate(
-                                {"rider_regNo" : regNo},
-                                {$set : {"bike_id" : doc.locks[0].bike_id }, $currentDate : {"date_time" : true}},
-                                {new : true}
-                            ).then((ele)=>{
-                                if(ele){
-                                    resolve({'response' : "success", 'res' : true});
-                                }else {
-                                    reject({'response' : "unsuccess not found rider", 'res' : false});
-                                }
-                            }).catch(()=>{reject({'response' : "unsuccess not found rider", 'res' : false})});
-                        }).catch(() => {reject({'response' : "unsuccess", 'res' : false})});
-                    }
-                }).catch(() => {reject({'response' :"unsuccess not found lock", 'res' : false})});
+                if(!doc.bike_id){
+                    mongoOp_station.findOne(
+                        {"locks.lock_id" : lockId},
+                        {"locks.$.bike_id" : true}
+                    ).then((doc) => {
+                        if(doc.locks[0].empty){
+                            reject({'response' : "unsuccess lock is already empty", 'res' : false});
+                        }else{
+                            mongoOp_station.findOneAndUpdate(
+                                {"locks.lock_id" : lockId},
+                                {$set : {"locks.$.bike_id" : null , "locks.$.empty" : true }, $inc:{'noOfEmpty' : 1, 'noOfBikes' : -1}},
+                                {upsert : true , fields : {"locks.$.bike_id" : 1, "station_id" : 1}}
+                            ).then((doc) => {
+                                mongoOp_bike.findOneAndUpdate(
+                                    {"rider_regNo" : regNo},
+                                    {$set : {"bike_id" : doc.locks[0].bike_id }, $currentDate : {"date_time" : true}},
+                                    {new : true}
+                                ).then((ele)=>{
+                                    if(ele){
+                                        resolve({'response' : "success", 'res' : true});
+                                    }else {
+                                        reject({'response' : "unsuccess not found rider", 'res' : false});
+                                    }
+                                }).catch(()=>{reject({'response' : "unsuccess not found rider", 'res' : false})});
+                            }).catch(() => {reject({'response' : "unsuccess", 'res' : false})});
+                        }
+                    }).catch(() => {reject({'response' :"unsuccess not found lock", 'res' : false})});
+                }else {
+                    reject({'response' :"You already have a bike", 'res' : false});
+                }
             }else {
                 reject({'response' :"unsuccess not found user", 'res' : false});
             }
