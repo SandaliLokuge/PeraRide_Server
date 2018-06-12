fe96c3588ed47066e9cc1e015473f818e3890d5a
shyaman_madhawa <shayaman321@gmail.com>
2018-06-13 05:24:19 +0530

    bug fix sms

--- functions/smsFetchStation.js
+++ functions/smsFetchStation.js
@@ -4,7 +4,7 @@ var smsfetchstation = () => {
     return new Promise((resolve, reject) => {
         mongoOp_station.find(
             {},
-            {_id:0, station_id:0, __v:0, location:0, locks:0},
+            {_id:0, station_id:0, __v:0, location:0, locks:0}
         ).then((res) => {
             resolve(res);
        }).catch((err) => {
