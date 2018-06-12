535f03155e67522f5efcd2c554c1890c575f8d11
Puwasuru Ihalagedara <puwasuru95@gmail.com>
2018-06-12 21:24:42 +0530

    getStation unset array

--- functions/getStationData.js
+++ functions/getStationData.js
@@ -2,7 +2,7 @@ const mongoOp_station = require('../models/stations');
 
 var stationdata = (name) => {
     return new Promise((resolve, reject) => {
-        mongoOp_station.find(
+        mongoOp_station.findOne(
             {'name' : name},
             {_id:0, station_id:0, __v:0, locks:0, location:0, name:0},
         ).then((res) => {
