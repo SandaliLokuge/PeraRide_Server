b966b4538076c99778a1338c2c7a1588e16514c2
Puwasuru Ihalagedara <puwasuru95@gmail.com>
2018-10-15 21:25:48 +0530

    change mqtt settings

--- config/mqtt.js
+++ config/mqtt.js
@@ -2,20 +2,22 @@ const mqtt = require ('mqtt');
 const lockBike = require('../functions/lockBike');
 const isnewbike = require('../functions/isNewBike');
 
-var client  = mqtt.connect('mqtt://159.89.238.212',{
-    port: 8883,
-    username: 'server',
-    password: 'perarideserver',
+var client  = mqtt.connect('m13.cloudmqtt.com',{
+    port: 14590,
+    username: 'qvpfzszg',
+    password: 'lPsmL_FZUtsh',
     clientId: 'peraride_server',
     rejectUnauthorized: false
 });
 
 client.on('connect', function() {
     client.subscribe('PeraRide/redock/lock2');
+    console.log('mqtt connected');
+    
 });
 
 client.on('message', function(topic,payload) { // When connected
-
+    console.log('a message has come');
     var body = {
         lock_id: topic.toString().replace('PeraRide/redock/lock',''),
         bike_id: payload.toString()
